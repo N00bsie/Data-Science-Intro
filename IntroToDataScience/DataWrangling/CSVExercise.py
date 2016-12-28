5e8a38968f247d60795f286bcf05a6e9c39fa333
N00bsie <semanticIyer@gmail.com>
2016-12-28 15:43:41 -0500

    Function (add_full_name) added. Takes in a csv file and adds a new column called 'nameFull', which is a combination of columns 'nameFirst' and 'nameLast'

--- IntroToDataScience/DataWrangling/CSVExercise.py
+++ IntroToDataScience/DataWrangling/CSVExercise.py
@@ -4,4 +4,19 @@ Created on Dec 28, 2016
 @author: AmarViswanathan
 '''
 
-print("Hello World!")
+import pandas as pd
+
+def add_full_name(path_to_csv,path_to_new_csv):
+    
+    baseball_data = pd.read_csv(path_to_csv)
+    
+    baseball_data['nameFull'] = baseball_data['nameFirst'] + " " + baseball_data['nameLast']
+    #Write back
+    baseball_data.to_csv(path_to_new_csv)
+    
+    
+if __name__ == "__main__":
+    path_to_csv = "../Master.csv"
+    path_to_new_csv = "../MasterNew.csv"
+    add_full_name(path_to_csv, path_to_new_csv)
+        
