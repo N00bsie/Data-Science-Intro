3641c03790ca6832ab58a4cdd689780e77de682b
N00bsie <semanticIyer@gmail.com>
2016-12-28 17:36:35 -0500

    Added a print message to the function.

--- IntroToDataScience/DataWrangling/CSVExercise.py
+++ IntroToDataScience/DataWrangling/CSVExercise.py
@@ -13,6 +13,7 @@ def add_full_name(path_to_csv,path_to_new_csv):
     baseball_data['nameFull'] = baseball_data['nameFirst'] + " " + baseball_data['nameLast']
     #Write back
     baseball_data.to_csv(path_to_new_csv)
+    print("File was written to " + path_to_new_csv)
     
     
 if __name__ == "__main__":
